7caa6eecc563ebdc15f1fa0421fb259a5a64d371
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2025-03-12 22:21:44 +0530

    Add files via upload
    Firrst Accourence of a  charecter

---
+++ first_Accurence_of_charector.java
@@ -0,0 +1,7 @@
+public class first_Accurence_of_charector {
+    public static void main(String[] args) {
+        String str="Programming is very funny";
+        char ch='g';
+        System.out.println(str.indexOf(ch));
+    }
+}
