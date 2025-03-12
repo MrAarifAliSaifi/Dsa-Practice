2b8a51d01c21417220293818b57773263a62ed97
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2025-03-12 14:34:59 +0530

    Add files via upload
    Palindrome string

---
+++ PalindromeString.java
@@ -0,0 +1,22 @@
+public class PalindromeString {
+    public static void main(String[] args) {
+        String str="oppo";
+        String reversedString="";
+
+        for(int i=str.length()-1;i>=0;i--){
+            reversedString=reversedString+str.charAt(i);
+        }
+        if(str.equals(reversedString)){
+            System.out.println("true");
+        }else {
+            System.out.println("false");
+        }
+        StringBuilder stringBuilder= new StringBuilder(str);
+        reversedString=stringBuilder.reverse().toString();
+        if(str.equals(reversedString)){
+            System.out.println("true");
+        }else {
+            System.out.println("false");
+        }
+    }
+}
