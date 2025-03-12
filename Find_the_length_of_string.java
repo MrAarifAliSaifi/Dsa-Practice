fee123c773145bf957209f2ca73ca358058e4aaa
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2025-03-12 14:45:28 +0530

    Add files via upload
    Length of a sting

---
+++ Find_the_length_of_string.java
@@ -0,0 +1,7 @@
+public class Find_the_length_of_string {
+    public static void main(String[] args) {
+        String str = "palindrome";
+        int len=str.length();
+        System.out.println(len);
+    }
+}
