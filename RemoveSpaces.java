49e0b2053d9525c21dfc64d61c5b6884164683c8
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2025-09-03 17:04:00 +0530

    Add files via upload

---
+++ RemoveSpaces.java
@@ -0,0 +1,9 @@
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        String str="My Name is Aarif Saif";
+        String SpacesLess="";
+        SpacesLess=str.replace(" ","");
+        System.out.println(SpacesLess);
+
+    }
+}
